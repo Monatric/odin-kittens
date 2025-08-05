f930f34f142f8f28fa85e2cef147b758ba9e5533
Monatric <nawpnawp8@gmail.com>
2025-08-05 19:59:39 +0800

    feat: add logic to actions in kittens

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,17 +1,29 @@
 class KittensController < ApplicationController
   def index
+    @kittens = Kitten.all
   end
 
   def show
+    @kitten = Kitten.find(params[:id])
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to @kitten, notice: "Kitten created successfully!"
+    else
+      flash.now[:alert] = "Kitten cannot be created."
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
@@ -19,4 +31,9 @@ class KittensController < ApplicationController
 
   def destroy
   end
+
+  private
+    def kitten_params
+      params.expect(kitten: [ :name, :age, :cuteness, :softness ])
+    end
 end
