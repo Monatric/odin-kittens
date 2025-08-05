bf99c8618f0c8076461391854bd33f09b0fd0728
Monatric <nawpnawp8@gmail.com>
2025-08-05 19:59:58 +0800

    fix: change kitten age's validation range

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,6 +1,6 @@
 class Kitten < ApplicationRecord
   validates :name, length: { in: 2..20 }
-  validates :age, comparison: { greater_than: 0, less_than: 20 }
+  validates :age, comparison: { greater_than: -1, less_than: 21 }
   enum :cuteness, { cute: 0, cutie: 1, overload: 2 }
   enum :softness, { soft: 0, tough: 1, unbreakable: 2 }
 end
