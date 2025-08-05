78236cee5f04712c85ba6f81368dd5a2fc411b86
Monatric <nawpnawp8@gmail.com>
2025-08-05 19:22:12 +0800

    fix: update cuteness enum in kitten

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,6 +1,6 @@
 class Kitten < ApplicationRecord
   validates :name, length: { in: 2..20 }
   validates :age, comparison: { greater_than: 0, less_than: 20 }
-  enum :cuteness, { good: 0, cutie: 1, overload: 2 }
+  enum :cuteness, { cute: 0, cutie: 1, overload: 2 }
   enum :softness, { soft: 0, tough: 1, unbreakable: 2 }
 end
